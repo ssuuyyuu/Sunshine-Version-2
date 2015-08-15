5dd51944c2255579a21737e392e4bdaf42111f00
Yu Su <yusu@hipagesgroup.com.au>
2015-08-15 22:31:32 +1000

    User shareActionProvider

--- app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -18,56 +20,30 @@ import android.widget.TextView;
  */
 public class DetailFragment extends Fragment {
 
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private String mForecastStr;
 
     public DetailFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Initialize the contents of the Activity's standard options menu.  You
-     * should place your menu items in to <var>menu</var>.  For this method
-     * to be called, you must have first called {@link #setHasOptionsMenu}.  See
-     * {@link Activity#onCreateOptionsMenu(Menu) Activity.onCreateOptionsMenu}
-     * for more information.
-     *
-     * @param menu     The options menu in which you place your items.
-     * @param inflater
-     * @see #setHasOptionsMenu
-     * @see #onPrepareOptionsMenu
-     * @see #onOptionsItemSelected
-     */
+    // Do not need to override onOptionsItemSelected. Once we set SharedIntent for shareActionProvider, it takes care of the action.
+
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.fragment_detail, menu);
-    }
 
-    /**
-     * This hook is called whenever an item in your options menu is selected.
-     * The default implementation simply returns false to have the normal
-     * processing happen (calling the item's Runnable or sending a message to
-     * its Handler as appropriate).  You can use this method for any items
-     * for which you would like to do processing without those other
-     * facilities.
-     * <p/>
-     * <p>Derived classes should call through to the base class for it to
-     * perform the default menu handling.
-     *
-     * @param item The menu item that was selected.
-     * @return boolean Return false to allow normal menu processing to
-     * proceed, true to consume it here.
-     * @see #onCreateOptionsMenu
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+        // find the share item
+        MenuItem menuItem = menu.findItem(R.id.action_share);
 
-        int id = item.getItemId();
+        // do not use menuItem.getActionProvider()
+        ShareActionProvider shareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
 
-        if (id == R.id.action_share) {
-
-            return true;
+        if (shareActionProvider != null) {
+            shareActionProvider.setShareIntent(createShareForecastIntent());
         }
 
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -81,9 +57,19 @@ public class DetailFragment extends Fragment {
         TextView textView = (TextView)rootView.findViewById(R.id.detail_textView);
 
         Intent intent = getActivity().getIntent();
-        textView.setText(intent.getStringExtra("extra"));
+        mForecastStr = intent.getStringExtra("extra");
+        textView.setText(mForecastStr);
 
         return rootView;
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,
+                mForecastStr + FORECAST_SHARE_HASHTAG);
+        return intent;
     }
 
 }
